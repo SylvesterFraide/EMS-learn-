f353f0c27a2f38b7aafdbe2a1748c7e0a4828581
SylvesterFraide <fridaysylvester2@gmail.com>
2026-01-18 11:03:39 +0300

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import  react from 'react'
+import react from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
-
   return (
-    <div className="container mx-auto">
-      <h1 className="text-4xl font-bold">Employee Management System</h1>
-      <div className="mt-4">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">
-          Add Employee
-        </button>
-      </div>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/admin-dashboard" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
