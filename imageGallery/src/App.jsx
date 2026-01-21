4d38bed88e37022fa18f142c359d3c911c346928
SylvesterFraide <fridaysylvester2@gmail.com>
2026-01-21 20:22:28 +0300

    setup

---
+++ imageGallery/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl font-bold text-teal-600">Welcome to the Image Gallery</h1>
+      <p className="mt-4">Explore our collection of beautiful images.</p>
+    </div>
+  )
+}
+
+export default App
