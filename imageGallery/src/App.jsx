ddf8b8bfabc4e37de71e3ce142718b62bf32a47e
SylvesterFraide <fridaysylvester2@gmail.com>
2026-01-24 16:38:05 +0300

    router

--- imageGallery/src/App.jsx
+++ imageGallery/src/App.jsx
@@ -1,47 +1,15 @@
-import { useEffect, useState } from "react";
-import ImageCard from "./components/imageCard";
-import Search from "./components/search";
+import React from "react";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 
-function App() {
-  const [images, setImages] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [term, setTerm] = useState("");
-
-  useEffect(() => {
-    fetch(
-      `https://pixabay.com/api/?key=${import.meta.env.VITE_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`,
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setImages(data.hits);
-        setIsLoading(false);
-      })
-      .catch((err) => console.log(err));
-  }, [term]);
-
+const App = () => {
   return (
-    <div className="container mx-auto">
-      <Search searchText={(text) => setTerm(text)} />
-
-      {!isLoading && images.length === 0 && (
-        <h3 className="text-gray-500 text-3xl font-bold item-center justify-center flex">
-          No images found.
-        </h3>
-      )}
-
-      {isLoading ? (
-        <h3 className="text-gray-500 text-5xl font-bold item-center justify-center flex">
-          Loading...
-        </h3>
-      ) : (
-        <div className="grid grid-cols-3 gap-4">
-          {images.map((image) => (
-            <ImageCard key={image.id} image={image} />
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ imageGallery/src/components/Home.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import ImageCard from "./imageCard";
+import Search from "./search";
+
+function Home() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${import.meta.env.VITE_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
+  return (
+    <div className="container mx-auto">
+      <Search searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && (
+        <h3 className="text-gray-500 text-3xl font-bold item-center justify-center flex">
+          No images found.
+        </h3>
+      )}
+
+      {isLoading ? (
+        <h3 className="text-gray-500 text-5xl font-bold item-center justify-center flex">
+          Loading...
+        </h3>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
