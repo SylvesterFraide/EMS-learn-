39541bb7e232f4d02b10d3d91b04a8990513a864
SylvesterFraide <fridaysylvester2@gmail.com>
2026-03-05 20:46:52 +0300

    set route, installing more packages

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./Component/Home";
 
 function App() {
   return (
-    <div className='bg-gray-900 min-h-screen flex items-center justify-center'>
-     <h1 className="text-3xl font-bold text-gray-400 text-center ">Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
