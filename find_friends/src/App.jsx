b2a0af9e3415ffbae8c7fe6150bbc4f80ce1fabc
SylvesterFraide <fridaysylvester2@gmail.com>
2026-03-10 20:25:17 +0300

    setting react-router

--- find_friends/src/App.jsx
+++ find_friends/src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Component/Home";
 
 function App() {
   return (
-<h1 className='p-6 text-red-500 text-4xl uppercase italic bg-red-100 text-center underline'>hello Jade en Jaylan</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ find_friends/src/Component/Data.js
@@ -0,0 +1,28 @@
+import jade from '../assets/Jade.jpg';
+import Jaylan from '../assets/Jaylan.jpg';
+
+export const Data = [
+  {
+    id: 1,
+    image: jade,
+    name: 'Jade',
+
+  },
+  {
+    id: 2,
+    image: Jaylan,
+    name: 'Jaylan',
+  },
+
+  {
+    id: 3,
+    image: 'Jade',
+    name: 'Rinah',
+  },
+
+  {
+    id: 4,
+    image: 'Jaylan',
+    name: 'Sylvester',
+  }
+]
