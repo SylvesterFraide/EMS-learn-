bc3faa1d7c027d64edce79ca8998f64b16275186
SylvesterFraide <fridaysylvester2@gmail.com>
2026-03-29 11:50:03 +0300

    dispaly table content on UI

--- react_data_table/src/component/data.js
+++ react_data_table/src/component/data.js
@@ -3,10 +3,24 @@ export const tableData = [
   { id: 2, name: "Jaylan Lamine", age: 34, email: "jaylan@example.com" },
   { id: 3, name: "Rinah Fraide", age: 45, email: "rinah@example.com" },
   { id: 4, name: "Sly Fraide", age: 23, email: "sly@example.com" },
+  { id: 5, name: "New User", age: 30, email: "newuser@example.com" },
+  { id: 6, name: "Another User", age: 40, email: "anotheruser@example.com" },
+  { id: 7, name: "Third User", age: 25, email: "thirduser@example.com" },
+  { id: 8, name: "Fourth User", age: 32, email: "fourthuser@example.com" },
+  { id: 9, name: "Fifth User", age: 29, email: "fifthuser@example.com" },
+  { id: 10, name: "Sixth User", age: 36, email: "sixthuser@example.com" },
+  { id: 11, name: "Seventh User", age: 27, email: "seventhuser@example.com" },
+  { id: 12, name: "Eighth User", age: 31, email: "eighthuser@example.com" },
+  { id: 13, name: "Ninth User", age: 29, email: "ninthuser@example.com" },
+  { id: 14, name: "Tenth User", age: 26, email: "tenthuser@example.com" },
+  { id: 15, name: "Eleventh User", age: 33, email: "eleventhuser@example.com" },
+  { id: 16, name: "Twelfth User", age: 30, email: "twelfthuser@example.com" },
+  { id: 17, name: "Thirteenth User", age: 28, email: "thirteenthuser@example.com" },
+  { id: 18, name: "Fourteenth User", age: 35, email: "fourteenthuser@example.com" }
 ];
 
 export const tableColumns = [
-  { name: "Name", selector: (row) => row.name },
-  { name: "Age", selector: (row) => row.age },
-  { name: "Email", selector: (row) => row.email },
+  { name: "Name", selector: (row) => row.name, sortable: true },
+  { name: "Age", selector: (row) => row.age, sortable: true },
+  { name: "Email", selector: (row) => row.email, sortable: true },
 ];
--- react_data_table/src/component/table.jsx
+++ react_data_table/src/component/table.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { tableData, tableColumns } from './data'
+import React from "react";
+import { tableData, tableColumns } from "./data";
+import * as RTD from "react-data-table-component";
 
 const Table = () => {
+  const DataTable = RTD.default;
+
   return (
-    <div className='min-h-screen flex items-center justify-center'>
-      <h1 className='text-4xl font-bold text-purple-600'>Table</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-purple-600">Table</h1>
+      <div className="w-3/4">
+        <DataTable.default columns={tableColumns} data={tableData}  
+        fixedHeader
+        pagination
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Table;
