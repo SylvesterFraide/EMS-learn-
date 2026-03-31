32216fa10b74d33048ef2725425263717cce54d3
SylvesterFraide <fridaysylvester2@gmail.com>
2026-03-31 21:19:45 +0300

    slice()

--- js/src/Component/Arrays/slice.js
+++ js/src/Component/Arrays/slice.js
@@ -3,8 +3,10 @@ import React from "react";
 const Slice = () => {
   const animals = ["ant", "bison", "camel", "duck", "elephant"];
 
-  const newarray = animals.slice(3);
+  const newarray = animals.slice(1,4);
+  const animalsCopy = animals.slice();
   console.log(newarray);
+  console.log(animalsCopy);
   // return (
   // );
 };
--- js/src/Component/Arrays/sort.js
+++ js/src/Component/Arrays/sort.js
@@ -1,15 +1,25 @@
 import React from "react";
 
 const Sort = () => {
-  const users = [
-    { id: 1, name: "AJade", age: 25 },
-    { id: 2, name: "Baylan", age: 30 },
-    { id: 3, name: "Cinah", age: 22 },
-  ];
-//   const sortedAge = users.sort((a, b) => a.age - b.age);
-  const sortedUsers = users.sort((a, b) => b.name.localeCompare(a.name));
-  console.log(sortedUsers);
+  const animals = ["elephant", "duck", "ant", "bison","camel"];
+  const numbers = [5, 3, 8, 1, 2];
 
+  const sortedAnimals = animals.sort();
+  console.log(sortedAnimals);
+
+
+  const sortedNumbers = numbers.sort((a,b) => a-b);
+  console.log(sortedNumbers);
+
+    const users = [
+      { id: 1, name: "AJade", age: 25 },
+      { id: 2, name: "Baylan", age: 30 },
+      { id: 3, name: "Cinah", age: 22 },
+    ];
+  const sortedAge = users.sort((a, b) => b.age - a.age);
+  const sortedUsers = users.sort((a, b) => b.name.localeCompare(a.name));
+  console.log(sortedAge);
+  console.log(sortedUsers);
   //   return (
   //     <div>Sort</div>
   //   )
