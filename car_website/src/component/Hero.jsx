c3cf6ca069b181000544916e5af2bc132c74a0c5
SylvesterFraide <fridaysylvester2@gmail.com>
2026-04-05 19:06:15 +0300

    update

--- car_website/src/component/Hero.jsx
+++ car_website/src/component/Hero.jsx
@@ -3,14 +3,14 @@ import HeroImg from '../assets/heroImg.avif';
 
 const Hero = () => {
   return (
-    <section className='w-[80vw] mx-auto flex justify-between items-center min-h-screen'>
-       <div className="">
+    <section className='w-[80vw] mx-auto flex flex-col md:flex-row justify-between items-center h-[90vh]'>
+       <div className="pt-6">
         <h1 className='text-xl font-bold text-purple-600'>Effortless</h1>
         <h2 className='text-6xl font-semibold capitalize my-4'>car rentals</h2>
         <p className='text-gray-600 text-xl'>Experience the best car rental services at your fingertips. </p>
         <button className='bg-purple-600 text-white py-2 px-4 rounded mt-4'>Get Started</button>
        </div>
-       <div className="">
+       <div className="pt-4">
          <img src={HeroImg} alt="Car Rental" className='h-auto w-150 object-cover' />
        </div>
     </section>
--- car_website/src/component/Navbar.jsx
+++ car_website/src/component/Navbar.jsx
@@ -16,8 +16,8 @@ const Navbar = () => {
     setIsDarkMode(!isDarkMode);
   };
   return (
-    <section className="w-full py-4 shadow-md fixed top-0 left-0 right-0 z-10">
-      <nav className="flex justify-between w-[80vw] mx-auto">
+    <section className="w-full h-[10vh] shadow-md">
+      <nav className="flex justify-between pt-5 items-center w-[80vw] mx-auto">
         <h2 className="text-2xl font-bold capitalize">car rental</h2>
 
         <div className="flex items-center space-x-6 ">
