20726644a910111ddd468b3e97d3dd550d6e65ee
SylvesterFraide <fridaysylvester2@gmail.com>
2026-04-06 10:42:48 +0300

    why choose us

---
+++ car_website/src/Pages/WhyChooseUs.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { RiPriceTag2Line } from "react-icons/ri";
+import { AiOutlineSafety } from "react-icons/ai";
+import { GrUserExpert } from "react-icons/gr";
+
+const WhyChooseUs = () => {
+  return (
+    <section className="w-[80vw] mx-auto bg-purple-100 h-screen">
+      <h1 className="text-4xl text-purple-600 text-center font-bold mb-4">
+        Why Choose Us?
+      </h1>
+      <div className="flex md:flex-row flex-col flex-wrap justify-evenly mt-10  ">
+        <div className="bg-black text-center text-white w-110 px-6 py-6 rounded-md shadow-md">
+          <RiPriceTag2Line className="text-6xl mx-auto my-4" />
+          <h2 className="text-3xl font-semibold my-6">Best Price</h2>
+          <p className="text-lg text-gray-300 mb-8">
+            Get unbeatable deals on a wide range of vehicles for every journey.
+            We ensure competitive pricing so you always drive away with more incredible
+            savings.
+          </p>
+
+          <a href="#" className="text-purple-400 hover:underline">
+            Learn More
+          </a>
+        </div>
+
+        <div className="bg-black text-center text-white w-110 px-6 py-6 rounded-md shadow-md">
+          <AiOutlineSafety className="text-6xl mx-auto my-4" />
+          <h2 className="text-3xl font-semibold my-6">Fast and safe</h2>
+          <p className="text-lg text-gray-300 mb-8">
+            Experience lightning-fast service without compromising safety. Our
+            vehicles undergo rigorous checks to ensure your peace of mind on
+            every journey.
+          </p>
+
+          <a href="#" className="text-purple-400 hover:underline">
+            Learn More
+          </a>
+        </div>
+
+        <div className="bg-black text-center text-white w-110 px-6 py-6 rounded-md shadow-md">
+          <GrUserExpert className="text-6xl mx-auto my-4" />
+          <h2 className="text-3xl font-semibold my-6">Experienced Drivers</h2>
+          <p className="text-lg text-gray-300 mb-8">
+            Our team of skilled drivers is dedicated to providing you with a
+            safe and enjoyable journey. With years of experience, we know the
+            best routes and ensure a smooth ride.
+          </p>
+
+          <a href="#" className="text-purple-400 hover:underline">
+            Learn More
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WhyChooseUs;
--- car_website/src/component/Home.jsx
+++ car_website/src/component/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
 import About from "../Pages/About";
+import WhyChooseUs from "../Pages/WhyChooseUs";
 
 const Home = () => {
   return (
     <main>
       <Hero />
       <About />
+      <WhyChooseUs />
     </main>
   );
 };
