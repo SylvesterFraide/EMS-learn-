b8e501e50c92e8c37225202ad186bcce57190b05
SylvesterFraide <fridaysylvester2@gmail.com>
2026-04-11 20:31:01 +0300

    vehicle data

--- car_website/src/component/HeroData.js
+++ car_website/src/component/HeroData.js
@@ -5,8 +5,155 @@ import heroImg6 from "../assets/heroImg6.webp";
 import heroImg7 from "../assets/heroImg7.webp";
 import heroImg8 from "../assets/heroImg8.jpg";
 
+import image1 from "../assets/image1.jpg";
+import image2 from "../assets/image2.jpg";
+import image3 from "../assets/image3.jpg";
+import image4 from "../assets/image4.jpg";
+import image5 from "../assets/image5.jpg";
+import image6 from "../assets/image6.jpg";
+import image7 from "../assets/image7.jpg";
+import image8 from "../assets/image8.jpg";
+import image9 from "../assets/image9.jpg";
+import image10 from "../assets/image10.jpg";
+import image11 from "../assets/image11.jpg";
+
 export const heroData = [
-  { text: "Best Price", description: "Get the best price for your ride", Image: heroImg7 },
-  { text: "Fast and Safe", description: "Your safety is our priority", Image: heroImg5 },
-  { text: "Experienced Drivers", description: "Professional drivers at your service", Image: heroImg4 },
+  {
+    text: "Best Price",
+    description: "Get the best price for your ride",
+    Image: heroImg7,
+  },
+  {
+    text: "Fast and Safe",
+    description: "Your safety is our priority",
+    Image: heroImg5,
+  },
+  {
+    text: "Experienced Drivers",
+    description: "Professional drivers at your service",
+    Image: heroImg4,
+  },
+];
+
+export const data = [
+  {
+    id: 1,
+    yeah: 2014,
+    make: "Mercedes Benz",
+    Model: "Mercedes-benz S-class Hybrid",
+    engine: "3500cc",
+    mileage: "37116km",
+    transmission: "AT",
+    image: image1,
+  },
+
+  {
+    id: 2,
+    yeah: 2016,
+    make: "Mercedes Benz",
+    model: "Mercedes-benz V-class",
+    engine: "2200cc",
+    mileage: "62869km",
+    transmission: "AT",
+    image: image2,
+  },
+
+  {
+    id: 3,
+    yeah: 2017,
+    make: "Toyota Voxy",
+    model: "Hybrid Zs 'kira 2'",
+    engine: "1800cc",
+    mileage: "123068km",
+    transmission: "AT",
+    image: image3,
+  },
+
+  {
+    id: 4,
+    yeah: 2018,
+    make: "Mercedes Benz",
+    model: "Mercedes-benz V-class",
+    engine: "2200cc",
+    mileage: "62899km",
+    transmission: "AT",
+    image: image4,
+  },
+
+  {
+    id: 5,
+    yeah: 2023,
+    make: "Toyota Voxy",
+    model: "Hybrid S-z",
+    engine: "1800cc",
+    mileage: "4129km",
+    transmission: "AT",
+    image: image5,
+  },
+
+  {
+    id: 6,
+    yeah: 2024,
+    make: "Toyota Noah",
+    model: "Hybrid S-z",
+    engine: "1800cc",
+    mileage: "22726km",
+    transmission: "AT",
+    image: image6,
+  },
+
+  {
+    id: 7,
+    yeah: 2024,
+    make: "Toyota Voxy",
+    model: "Hybrid S-z",
+    engine: "1800cc",
+    mileage: "6833km",
+    transmission: "AT",
+    image: image7,
+  },
+
+  {
+    id: 8,
+    yeah: 2025,
+    make: "Toyota Noah",
+    model: "Hybrid 1.8 Hybrid S-z",
+    engine: "1800cc",
+    mileage: "8km",
+    transmission: "AT",
+    image: image8,
+  },
+
+  {
+    id: 9,
+    yeah: 2025,
+    make: "Toyota Noah",
+    model: "Hybrid 4wd Hybrid S-z",
+    engine: "1800cc",
+    mileage: "7100km",
+    transmission: "AT",
+    image: image9,
+  },
+
+  {
+    id: 10,
+    yeah: 2026,
+    make: "Toyota Noah",
+    model: "Hybrid S-z",
+    engine: "1800cc",
+    mileage: "9km",
+    transmission: "AT",
+    image: image10,
+  },
+
+  {
+    id: 11,
+    yeah: 2026,
+    make: "Toyota Noah",
+    model: "Hybrid X",
+    engine: "1800cc",
+    mileage: "20km",
+    transmission: "AT",
+    image: image11,
+  },
 ];
