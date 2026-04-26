34ecc47843855471ca2b99df490ac397de195681
SylvesterFraide <fridaysylvester2@gmail.com>
2026-04-26 23:31:06 +0300

    add dependencies

--- jabra/src/App.jsx
+++ jabra/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
     <section>
-      <h1>jabra landscaping</h1>
+      <h1 className='text-4xl font-bold text-center mt-6 text-pink-600/40'>jabra landscaping</h1>
     </section>
     </>
   )
