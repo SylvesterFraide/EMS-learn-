ea9b9397a4038bd523088d5d0166aae46eacdd5c
SylvesterFraide <fridaysylvester2@gmail.com>
2026-04-30 23:07:21 +0300

    hero section

---
+++ jabra/src/component/Hero.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { HeroServices } from "./NavItems";
+
+const Hero = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const nextSlide = () => {
+    if (slideIndex < HeroServices.length - 1) {
+      setSlideIndex((prev) => prev + 1);
+    } else {
+      setSlideIndex(0);
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 3000);
+    return () => clearInterval(interval); // clear when the component unmount
+  }, [nextSlide]);
+
+  return (
+    <section
+      className="w-full relative mx-auto h-[80vh] opacity-90 shadow-lg mb-20 transition-all duration-500"
+      style={{
+        backgroundImage: `url(${HeroServices[slideIndex].image})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="absolute top-30 items-start h-full p-8 text-white">
+        {/* <h1 className="text-xl font-bold text-pink-600">Effortless</h1> */}
+        <h2 className="text-6xl font-semibold capitalize my-4">
+          {HeroServices[slideIndex].text}
+        </h2>
+        <p className="text-xl">{HeroServices[slideIndex].desc}</p>
+
+        <div className=" flex items-center space-x-3 mt-10 ">
+          {HeroServices.map((item, index) => (
+            <span
+              key={index}
+              onClick={() => setSlideIndex(index)}
+              className={`bg-green-700/80 cursor-pointer h-3 w-3 rounded-full ${index === slideIndex && "bg-white"}`}
+            ></span>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- jabra/src/component/NavItems.js
+++ jabra/src/component/NavItems.js
@@ -1,6 +1,44 @@
+import image1 from '../assets/Images/jabra1.jpg';
+import image2 from '../assets/Images/jabra2.jpg';
+import image3 from '../assets/Images/jabra3.jpg';
+import image4 from '../assets/Images/jabra4.jpg';
+import image5 from '../assets/Images/jabra5.jpg';
+
 export const NavItems = [
   { name: "Home", path: "/" },
   { name: "About", path: "/about" },
   { name: "Services", path: "/services" },
   { name: "Contact", path: "/contact" },
 ];
+
+export const HeroServices = [
+  {
+    text: 'Flower Designs',
+    desc: 'Beautiful flower arrangements for any occasion.',
+    image: image1,
+  },
+
+    {
+    text: 'Grass Planting',
+    desc: 'Expert grass planting services for a lush lawn.',
+    image: image2,
+  },
+
+    {
+    text: 'Cabro Layout',
+    desc: 'Durable cabro stones for a beautiful driveway.',
+    image: image3,
+  },
+
+    {
+    text: 'Gardening Planning & Maintenance',
+    desc: 'Comprehensive gardening services for a thriving landscape.',
+    image: image4,
+  },
+
+    {
+    text: 'Landscaping Training',
+    desc: 'Hands-on training for effective landscaping techniques.',
+    image: image5,
+  },
+]
