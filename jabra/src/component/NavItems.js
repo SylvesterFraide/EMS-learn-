6755dd29e5ac9820b21463613f41f5cdcc88f99a
SylvesterFraide <fridaysylvester2@gmail.com>
2026-05-19 19:37:49 +0300

    updates on hero

--- jabra/src/component/NavItems.js
+++ jabra/src/component/NavItems.js
@@ -4,6 +4,12 @@ import image3 from '../assets/Images/jabra3.jpg';
 import image4 from '../assets/Images/jabra8.jpg';
 import image5 from '../assets/Images/jabra5.jpg';
 
+import cabro6 from '../assets/Images/cabro6.jpg'
+import training6 from '../assets/Images/training6.jpg'
+import lawn2 from '../assets/Images/lawn2.jpg'
+import Planning6 from '../assets/Images/Planning6.jpg'
+import training1 from '../assets/Images/training2.jpg'
+
 export const NavItems = [
   { name: "Home", path: "/",  },
   { name: "About", path: "/about" },
@@ -15,30 +21,30 @@ export const HeroServices = [
   {
     text: 'Flower Designs',
     desc: 'Beautiful flower arrangements for any occasion.',
-    image: image1,
+    image: training6,
   },
 
     {
     text: 'Grass Planting',
     desc: 'Expert grass planting services for a lush lawn.',
-    image: image2,
+    image: lawn2,
   },
 
     {
     text: 'Cabro Layout',
     desc: 'Durable cabro stones for a beautiful driveway.',
-    image: image3,
+    image: cabro6,
   },
 
     {
     text: 'Gardening Planning & Maintenance',
     desc: 'Comprehensive gardening services for a thriving landscape.',
-    image: image4,
+    image: Planning6,
   },
 
     {
     text: 'Landscaping Training',
     desc: 'Hands-on training for effective landscaping techniques.',
-    image: image5,
+    image: training1,
   },
 ]
