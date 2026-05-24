1e2e29dbaec2f84a6e5f71346af58216d37ff87e
SylvesterFraide <fridaysylvester2@gmail.com>
2026-05-24 23:11:35 +0300

    updates on service page

--- jabra/src/pages/Product.jsx
+++ jabra/src/pages/Product.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { services } from "../component/services";
+import { FaStar } from "react-icons/fa";
 
 const Product = () => {
   const { id } = useParams();
@@ -25,30 +26,42 @@ const Product = () => {
     fetchProduct();
   }, [id]);
 
-  console.log(productData);
-  console.log(image);
-
   return (
-    <div className="flex gap-12 sm:gap-12 flex-col md:flex-row w-[90vw] mx-auto mt-10 h-full">
-      <div className="flex-1 flex flex-col-reverse gap-3 md:flex-row">
-        <div className="flex md:flex-col overflow-x-scroll md:overflow-x-hidden md:overflow-y-scroll justify-between sm:justify-normal md:w-[18.7%] w-full ">
+    <div className="flex gap-12 sm:gap-12 flex-col md:flex-row w-[90vw] mx-auto my-12 h-full">
+      <div className="flex flex-col-reverse gap-3 md:flex-row bg-gray-100 p-4 rounded-md shadow-md">
+        <div className="flex md:flex-col overflow-x-scroll md:overflow-x-hidden overflow-y-scroll-hidden justify-between sm:justify-normal md:w-[18.7%] w-full ">
           {productData?.image?.map((img, index) => (
             <img
               onMouseEnter={() => setImage(img)}
               key={index}
               src={img}
               alt=""
-              className="w-[24%] md:w-full md:mb-3 flex-shrink-0 cursor-pointer"
+              className="w-[24%] md:w-full md:mb-3 flex-shrink-0 cursor-pointer rounded-md "
             />
           ))}
         </div>
         <div className="w-full md:w-[80%]">
-          <img src={image} alt="" className="w-full h-auto" />
+          <img src={image} alt="" className="w-full h-auto rounded-md" />
         </div>
       </div>
 
-      <div className="flex-1">
-        <h2 className="text-xl font-bold">{productData?.name}</h2>
+      <div className="mb-12 bg-gray-100 p-4 rounded-md shadow-md">
+        <h2 className="text-2xl font-bold text-gray-600">
+          {productData?.title}
+        </h2>
+        <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 my-6">
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-500" />
+            <FaStar className="text-yellow-300" />
+          </div>
+          <p className="text-gray-500">(4.0)</p>
+        </div>
+
+        <p className="text-gray-500 my-6">{productData?.description}</p>
+        <p className="text-gray-500 my-6">{productData?.desc}</p>
       </div>
     </div>
   );
