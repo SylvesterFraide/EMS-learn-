c6a248698e1ee04df5df5189dc9ffb9d5698640b
SylvesterFraide <fridaysylvester2@gmail.com>
2026-06-01 00:26:51 +0300

    updates on footer

--- jabra/src/component/Footer.jsx
+++ jabra/src/component/Footer.jsx
@@ -1,37 +1,114 @@
 import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="text-white w-full">
-      <div className="bg-gray-700 py-2 h-auto]">
-        <div className="flex flex-col">
-          <p>logo</p>
-          <p>
+    <div className="text-white w-full ">
+      <div className="flex flex-row md:flex-row bg-gray-700 py-2 px-14 h-auto gap-4">
+        <div className="flex flex-col w-[30rem]">
+          <p className="text-2xl font-bold text-green-600 uppercase">logo</p>
+          <p className="text-base text-gray-300 mt-4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
             dignissimos assumenda consequatur totam, error provident fuga velit
             officia,
           </p>
         </div>
-        <div className="">
-          <h2>useful links</h2>
-          <div className="">
+        <div className="flex-1">
+          {" "}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-2 p-4">
+            <div>
+              <h2 className="text-lg font-semibold mt-4">useful links</h2>
+              <div className="my-4">
+                <div className="flex items-center space-x-2">
+                  <MdKeyboardDoubleArrowRight />
+                  <Link className="hover:underline hover:text-green-500" to="/">
+                    Home
+                  </Link>
+                </div>
+
+                <div className="flex items-center space-x-2">
+                  <MdKeyboardDoubleArrowRight />
+                  <Link
+                    className="hover:underline hover:text-green-500"
+                    to="/about"
+                  >
+                    About
+                  </Link>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <MdKeyboardDoubleArrowRight />
+                  <Link
+                    className="hover:underline hover:text-green-500"
+                    to="/services"
+                  >
+                    Services
+                  </Link>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <MdKeyboardDoubleArrowRight />
+                  <Link
+                    className="hover:underline hover:text-green-500"
+                    to="/contact"
+                  >
+                    Contact
+                  </Link>
+                </div>
+              </div>
+            </div>
             <div className="">
-              <MdKeyboardDoubleArrowRight />
-              <Link to="/">Home</Link>
+              <h2 className="text-lg font-semibold">Follow us</h2>
+              <div className="flex space-x-4 my-4">
+                <Link
+                  className="hover:underline hover:text-green-500"
+                  to="/facebook"
+                >
+                  <FaFacebook className="text-xl text-gray-300 hover:text-green-500" />
+                </Link>
+                <Link
+                  className="hover:underline hover:text-green-500"
+                  to="/twitter"
+                >
+                  <FaTwitter className="text-xl text-gray-300 hover:text-green-500" />
+                </Link>
+                <Link
+                  className="hover:underline hover:text-green-500"
+                  to="/instagram"
+                >
+                  <FaInstagram className="text-xl text-gray-300 hover:text-green-500" />
+                </Link>
+              </div>
             </div>
 
             <div className="">
-              <MdKeyboardDoubleArrowRight />
-              <Link to="/about">About</Link>
-            </div>
-            <div className="">
-              <MdKeyboardDoubleArrowRight />
-              <Link to="/services">Services</Link>
-            </div>
-            <div className="">
-              <MdKeyboardDoubleArrowRight />
-              <Link to="/contact">Contact</Link>
+              <h2 className="text-lg font-semibold">Our Services</h2>
+              <ul className="list-disc list-inside mt-2">
+                <li>
+                  <Link
+                    className="hover:underline hover:text-green-500"
+                    to="/landscaping"
+                  >
+                    Landscaping
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="hover:underline hover:text-green-500"
+                    to="/design"
+                  >
+                    Design
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="hover:underline hover:text-green-500"
+                    to="/maintenance"
+                  >
+                    Maintenance
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
