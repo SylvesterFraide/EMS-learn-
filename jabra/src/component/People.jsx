401ae44b1d315bf92f3d7119ac6f785874b2d6d3
SylvesterFraide <fridaysylvester2@gmail.com>
2026-06-14 18:12:23 +0300

    home_why choose us

---
+++ jabra/src/component/People.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+const People = () => {
+  return (
+    <main className="w-full h-auto bg-green-100/80 my-30 p-6">
+      <div className="w-[80vw] mx-auto">
+        <div className="flex md:flex-row flex-col space-x-20">
+          <div className="p-4">
+            <h3 className="text-green-600 text-lg capitalize font-semibold mb-2">
+              why choose us
+            </h3>
+            <p className="capitalize text-2xl font-bold">
+              We create beautiful <br /> outdoor experience
+            </p>
+          </div>
+          <div className="flex-1">
+            <div className="grid grid-cols-2 md:grid-cols-4 p-4 gap-8">
+              <div className="flex flex-col items-center border-r border-green-300 pr-2">
+                <p>icon</p>
+                <h4 className="text-green-600 text-lg capitalize font-semibold mb-2">
+                  experienced team
+                </h4>
+                <p className="text-gray-600 text-center text-sm">
+                  Skilled professionals with years of experience.
+                </p>
+              </div>
+              <div className="flex flex-col items-center border-r border-green-300 pr-2">
+                <p>icon</p>
+                <h4 className="text-green-600 text-lg capitalize font-semibold mb-2">
+                  eco-friendly
+                </h4>
+                <p className="text-gray-600 text-center text-sm">
+                  Environmentally friendly practices and solutions.
+                </p>
+              </div>
+              <div className="flex flex-col items-center border-r border-green-300 pr-2">
+                <p>icon</p>
+                <h4 className="text-green-600 text-lg capitalize font-semibold mb-2">
+                  quality work
+                </h4>
+                <p className="text-gray-600 text-center text-sm">
+                  We deliver high-quality work that exceeds expectations.
+                </p>
+              </div>
+              <div className="flex flex-col items-center pr-2">
+                <p>icon</p>
+                <h4 className="text-green-600 text-lg capitalize font-semibold mb-2 ">
+                  affordable pricing
+                </h4>
+                <p className="text-gray-600 text-center text-sm">
+                  Competitive prices with transparent estimates.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default People;
--- jabra/src/pages/Home.jsx
+++ jabra/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../component/Hero';
 import Rating from '../component/Rating';
 import HomeServices from '../component/HomeServices';
+import People from '../component/People';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
      <Hero />
      <Rating />
      <HomeServices />
+     <People />
    </section>
   )
 }
