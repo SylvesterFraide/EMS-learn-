fa50d19fc93f3fb16770bf01ca1e07928b8ed4cf
SylvesterFraide <fridaysylvester2@gmail.com>
2026-07-05 11:57:29 +0300

    Next.Js learn

---
+++ my-portfolio/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="bg-gray-100 p-8">
+      <h1 className="text-2xl font-bold mb-4">About Us</h1>
+      <p className="text-gray-700">Welcome to our about page!</p>
+    </div>
+  );
+}
