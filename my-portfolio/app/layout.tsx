d775cf045bb512d35ad227a24635f37b8974da21
SylvesterFraide <fridaysylvester2@gmail.com>
2026-07-05 12:42:36 +0300

    simple navbar in next.js

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <div className="flex justify-end p-4 gap-4 bg-gray-200">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
