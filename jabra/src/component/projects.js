be1cc1425614d1afc961665d17da9c0d7a436f98
SylvesterFraide <fridaysylvester2@gmail.com>
2026-08-03 21:56:02 +0300

    add project details

--- jabra/src/component/projects.js
+++ jabra/src/component/projects.js
@@ -23,3 +23,132 @@ import peponi from '../assets/projectImg/peponi.jpg';
 import runda from '../assets/projectImg/runda.jpg';
 import runda2 from '../assets/projectImg/runda.jpg';
 import runda3 from '../assets/projectImg/training.jpg';
+
+
+export const project = [
+    {
+        id: 1,
+        title: "Commercial Project 1",
+        image: commercial1
+    },
+    {
+        id: 2,
+        title: "Commercial Project 2",
+        image: commercial2
+    },
+    {
+        id: 3,
+        title: "Commercial Project 3",
+        image: commercial3
+    },
+    {
+        id: 4,
+        title: "Home Project 1",
+        image: home1
+    },
+    {
+        id: 5,
+        title: "Home Project 2",
+        image: home2
+    },
+    {
+        id: 6,
+        title: "Home Project 3",
+        image: home3
+    },
+    {
+        id: 7,
+        title: "Home Project 4",
+        image: home4
+    },
+    {
+        id: 8,
+        title: "Home Project 5",
+        image: home5
+    },
+    {
+        id: 9,
+        title: "Home Project 6",
+        image: home6
+    },
+    {
+        id: 10,
+        title: "Home Project 7",
+        image: home7
+    },
+    {
+        id: 11,
+        title: "Home Project 8",
+        image: home8
+    },
+    {
+        id: 12,
+        title: "Home Project 9",
+        image: home9
+    },
+    {
+        id: 13,
+        title: "Home Project 10",
+        image: home10
+    },
+    {
+        id: 14,
+        title: "Home Project 11",
+        image: home11
+    },
+    {
+        id: 15,
+        title: "Home Project 12",
+        image: home12
+    },
+    {
+        id: 16,
+        title: "Hotel Project",
+        image: hotel
+    },
+    {
+        id: 17,
+        title: "Hotel Project 2",
+        image: hotel2
+    },
+    {
+        id: 18,
+        title: "Hotel Project 3",
+        image: hotel3
+    },
+    {
+        id: 19,
+        title: "Karen Project",
+        image: karen
+    },
+    {
+        id: 20,
+        title: "Karen Project 2",
+        image: karen2
+    },
+    {
+        id: 21,
+        title: "Loresho Project",
+        image: loresho
+    },
+    {
+        id: 22,
+        title: "Peponi Project",
+        image: peponi
+    },
+    {
+        id: 23,
+        title: "Runda Project",
+        image: runda
+    },
+    {
+        id: 24,
+        title: "Runda Project 2",
+        image: runda2
+    },
+    {
+        id: 25,
+        title: "Runda Project 3",
+        image: runda3
+    }
+]
