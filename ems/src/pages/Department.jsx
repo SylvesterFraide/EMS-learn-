bedc33569211692120da1391562cc35c0390e491
SylvesterFraide <fridaysylvester2@gmail.com>
2026-09-19 22:16:35 +0300

    updates on departments

--- ems/src/pages/Department.jsx
+++ ems/src/pages/Department.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../Context/useContext";
 
@@ -12,10 +12,11 @@ const Department = () => {
       <h2 className="text-center font-bold text-3xl macondo-regular">
         Manage Departments
       </h2>
+
       <form action="" className="flex justify-between py-6 px-10">
         <input
           type="text"
-          className="bg-white py-2 px-5 cursor-pointer rounded-lg shadow-xl outline-none border border-dotted border-teal-600"
+          className="bg-white py-2 px-5 rounded-lg shadow-xl outline-none border border-dotted border-teal-600"
           placeholder="Search by dep name"
         />
         <Link
@@ -26,6 +27,7 @@ const Department = () => {
           <p className="text-xl">New Department</p>
         </Link>
       </form>
+
       <table className="min-w-2/4 bg-white border border-red-300 mx-auto">
         <thead>
           <tr className="bg-gray-300">
@@ -36,27 +38,15 @@ const Department = () => {
           </tr>
         </thead>
         <tbody>
-          {department.map((item) => (
+          {department?.map((item) => (
             <tr key={item.id}>
               <td className="border border-gray-400 px-4 py-2">{item.id}</td>
-              <td className="border border-gray-400 px-4 py-2">
-                {item.Department}
-              </td>
-              <td className="border border-gray-400 px-4 py-2">
-                {item.Description}
-              </td>
-              <td className="space-x-4 border border-gray-400 px-12 py-2">
-                <button className="bg-yellow-500 hover:bg-yellow-400 text-white px-2 py-1 cursor-pointer rounded">
-                  View
-                </button>
-
-                <button className="bg-yellow-500 hover:bg-yellow-400 text-white px-2 py-1 cursor-pointer rounded">
-                  Update
-                </button>
-
-                <button
-                  onClick={() => deleteDepartment(item.id)}
-                  className="bg-red-500 hover:bg-red-400 text-white px-2 py-1 cursor-pointer rounded">
+              <td className="border border-gray-400 px-4 py-2">{item.Department}</td>
+              <td className="border border-gray-400 px-4 py-2">{item.Description}</td>
+              <td className="space-x-2 border border-gray-400 px-4 py-2 text-center">
+                <button className="bg-blue-500 hover:bg-blue-400 text-white px-2 py-1 rounded">View</button>
+                <button className="bg-yellow-500 hover:bg-yellow-400 text-white px-2 py-1 rounded">Update</button>
+                <button onClick={() => deleteDepartment(item.id)} className="bg-red-500 hover:bg-red-400 text-white px-2 py-1 rounded">
                   Delete
                 </button>
               </td>
@@ -64,48 +54,6 @@ const Department = () => {
           ))}
         </tbody>
       </table>
-
-      {/* updateDepartment */}
-
-         <table className="min-w-2/4 bg-white border border-red-300 mx-auto">
-        <thead>
-          <tr className="bg-gray-300">
-            <th className="border border-gray-400 py-3">Id</th>
-            <th className="border border-gray-400 py-3">Department</th>
-            <th className="border border-gray-400 py-3">Description</th>
-            <th className="border border-gray-400 py-3">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {department.map((item) => (
-            <tr key={item.id}>
-              <td className="border border-gray-400 px-4 py-2">{item.id}</td>
-              <td className="border border-gray-400 px-4 py-2">
-                {item.Department}
-              </td>
-              <td className="border border-gray-400 px-4 py-2">
-                {item.Description}
-              </td>
-              <td className="space-x-4 border border-gray-400 px-12 py-2">
-                <button className="bg-yellow-500 hover:bg-yellow-400 text-white px-2 py-1 cursor-pointer rounded">
-                  View
-                </button>
-
-                <button className="bg-yellow-500 hover:bg-yellow-400 text-white px-2 py-1 cursor-pointer rounded">
-                  Update
-                </button>
-
-                <button
-                  onClick={() => deleteDepartment(item.id)}
-                  className="bg-red-500 hover:bg-red-400 text-white px-2 py-1 cursor-pointer rounded">
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
     </div>
   );
 };
